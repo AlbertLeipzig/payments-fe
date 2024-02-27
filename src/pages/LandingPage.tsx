576779affea9b7b9a170cc5bb027c74aa88b2ae9
AlbertLeipzig <albertclemvill@gmail.com>
2024-02-27 16:49:54 +0100

    wip

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,31 +1,15 @@
 import { useContext } from 'react';
 import { AppContext } from '../utils/AppContext';
+import { ProductCard } from '../components/ProductCard';
 export const LandingPage = () => {
-  const { products, cart, setCart } = useContext(AppContext);
-
-  const formatDescription = (description: string) => {
-    return description.length <= 100
-      ? description
-      : `${description.slice(0, 100)}...`;
-  };
-
-  const addToCart = (product: any) => {
-    setCart([...cart, product]);
-  };
+  const { products } = useContext(AppContext);
 
   return (
     <div className="landing-page">
       <h1>Landing Page</h1>
-      {products &&
-        products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <h3>{product.productName}</h3>
-            <p>{product.price} €</p>
-            <p>{product.company}</p>
-            <p>{formatDescription(product.productDescription)}</p>
-            <button onClick={() => addToCart(product)}>Buy</button>
-          </div>
-        ))}
+      <div className="landing-page__products-container">
+        {products && products.map((product) => <ProductCard {...product} />)}
+      </div>
     </div>
   );
 };
