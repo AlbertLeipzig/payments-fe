b08396e992eff7eaf7ce0f926264c4da5f0303f2
AlbertLeipzig <albertclemvill@gmail.com>
2024-02-27 10:45:33 +0100

    wip

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,23 @@
+import { IProduct } from '../utils/types';
+import { useContext } from 'react';
+import { AppContext } from '../utils/AppContext';
+
+export const ProductCard = (product: IProduct) => {
+  const { cart, setCart } = useContext(AppContext);
+  const addToCart = (product: IProduct) => {
+    setCart([...cart, product]);
+  };
+  const removeFromCart = (product: IProduct) => {
+    return cart.filter((_product) => _product.id !== product.id);
+  };
+
+  return (
+    <div className="product-card" key={product.id}>
+      <h3>{product.productName}</h3>
+      <p>{product.price} €</p>
+      <p>{product.company}</p>
+      <button onClick={() => addToCart(product)}>Buy</button>
+      <button onClick={() => removeFromCart(product)}>Remove from Cart</button>
+    </div>
+  );
+};
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,7 +1,21 @@
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../utils/AppContext';
+import { ProductCard } from '../components/ProductCard';
+
 export const CartPage = () => {
-    return(
-        <div className="cart-page">
-            <h1>Payment Page</h1>
-        </div>
-    )
-}
+  const { cart } = useContext(AppContext);
+
+
+
+  
+  return (
+    <div className="cart-page">
+      <h1>Cart Page</h1>
+      {cart.length <= 0 ? (
+        <p>your cart is empty</p>
+      ) : (
+        cart.map((product) => <ProductCard {...product} />)
+      )}
+    </div>
+  );
+};
